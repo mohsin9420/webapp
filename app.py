06275f2e6378fee0190ee60f59ae21bf1548b7c3
mohsin9420 <87065826+mohsin9420@users.noreply.github.com>
2021-10-18 08:55:25 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,9 @@
+from logging import debug
+from flask import Flask 
+app = Flask (__name__)
+
+@app.route("/<username>")
+def greet(username):
+    return f"Hello,{username}"
+if __name__=="__main__":
+        app.run(debug=True)
