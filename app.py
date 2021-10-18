7dbf068429be21e8aa340015eb50c6a05df9e9fc
mohsin9420 <87065826+mohsin9420@users.noreply.github.com>
2021-10-18 10:37:53 +0530

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-from logging import debug
-from flask import Flask 
-app = Flask (__name__)
-
-@app.route("/<username>")
-def greet(username):
-    return f"Hello,{username}"
-if __name__=="__main__":
-        app.run(debug=True)
